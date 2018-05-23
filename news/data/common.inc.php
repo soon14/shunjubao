ac2e8d69431e4894840b78067c968d114eb3c8df
tangyihang <tangyihangchina@163.com>
2018-05-23 21:57:27 +0800

    数据库连接修改

--- news/data/common.inc.php
+++ news/data/common.inc.php
@@ -3,7 +3,7 @@
 $cfg_dbhost = '127.0.0.1';
 $cfg_dbname = 'dedecms';
 $cfg_dbuser = 'root';
-$cfg_dbpwd = 'meijun820526^&LKASI';
+$cfg_dbpwd = '1q2w3e4R!';
 $cfg_dbprefix = 'dede_';
 $cfg_db_language = 'utf8';
 
